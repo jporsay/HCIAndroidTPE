bb4266169c04c4c67f4ddc9fe9743750f35ff676
Juan Pablo Orsay <jporsay@gmail.com>
2011-06-05 23:29:12 -0300

    made login better

--- src/com/grupo3/productConsult/activities/LoginActivity.java
+++ src/com/grupo3/productConsult/activities/LoginActivity.java
@@ -1,14 +1,24 @@
 package com.grupo3.productConsult.activities;
 
-import com.grupo3.productConsult.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.grupo3.productConsult.R;
 
 public class LoginActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+	}
+	
+	public void doLogin(View button) {
+		final EditText username = (EditText) findViewById(R.id.usernameInput);
+		final EditText password = (EditText) findViewById(R.id.passwordInput);
+		
+		String name = username.getText().toString();
+		String passwd = password.getText().toString();
 	}
 }
