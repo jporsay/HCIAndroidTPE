e49792fa8363b0bf2451363377b76dcde982f33b
Juan Pablo Orsay <jporsay@gmail.com>
2011-06-07 02:06:48 -0300

    added initial ParsedXMLResponse class.

---
+++ src/com/grupo3/productConsult/utilities/ParsedXMLResponse.java
@@ -0,0 +1,11 @@
+package com.grupo3.productConsult.utilities;
+
+public class ParsedXMLResponse {
+	private String status;
+	private String error;
+	
+	//This class will contain parsed XML from Services.
+	public ParsedXMLResponse() {
+		// TODO Auto-generated constructor stub
+	}
+}
