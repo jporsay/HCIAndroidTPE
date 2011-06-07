493aa4a32fa3cef5f5370052c2d1692b1cb04a39
gonzalo.castiglione <gonzalo.castiglione@avature.net>
2011-06-07 10:11:32 -0300

    creados categories.xml y CategorySelectionActivity.java

---
+++ src/com/grupo3/productConsult/activities/CategorySelectionActivity.java
@@ -0,0 +1,13 @@
+package com.grupo3.productConsult.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CategorySelectionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
