8432a4c43b7bda6df6d3d7f43d941087e5c6319f
gonzalo.castiglione <gonzalo.castiglione@avature.net>
2011-06-10 04:13:24 -0300

    actualizando CategoryManager para que funcione de acuerdo al correcto flujo de datos.

--- src/com/grupo3/productConsult/CategoryManager.java
+++ src/com/grupo3/productConsult/CategoryManager.java
@@ -11,22 +11,22 @@ public class CategoryManager {
 	private static CategoryManager instance = null;
 	private List<Category> categoryList;
 
-	public static CategoryManager getInstance() throws ClientProtocolException, IOException {
+	public static CategoryManager getInstance() {
 		if (instance == null) {
 			instance = new CategoryManager();
 		}
 		return instance;
 	}
 
-	private CategoryManager() throws ClientProtocolException, IOException {
-		categoryList = CategoriesSearchService.fetchCategories();
+	private CategoryManager() {
 	}
 
 	public String[] getCategoryNames() {
 		return getNames(categoryList);
 	}
 
-	public String[] getSubCategoryNames(int index) throws ClientProtocolException, IOException {
+	public String[] getSubCategoryNames(int index)
+			throws ClientProtocolException, IOException {
 		Category c = categoryList.get(index);
 		if (c.getSubCategories().isEmpty()) {
 			int id = c.getId();
@@ -48,4 +48,9 @@ public class CategoryManager {
 		return categoryList;
 	}
 
+	// ------------------------------------------
+
+	public void saveCategoryList(List<Category> categories) {
+		this.categoryList = categories;
+	}
 }
